068bbc88f56b678a4a854d947ba7dcabb5ec414d
ed <ejbyne@gmail.com>
2014-12-11 19:18:14 +0000

    added left chatroom message

--- src/chatController.js
+++ src/chatController.js
@@ -2,23 +2,29 @@ var socket = function(io, session) {
 
   io.on('connection', function(socket){
 
-    console.log('Client connected!');
-
-    socket.on('disconnect', function() {
-      console.log('Client disconnected!');
-    });
-
-    socket.on('chat message', function(msg) {
-      io.emit('chat message', msg);
-    });
+    console.log('A user connected!');
 
     socket.on('username', function(username) {
       session.user = { name: username };
-      console.log(session.user)
+      console.log(session.user.name + ' joined the chatroom!');
       io.emit('chat message', {
         user: username,
         text: " joined the chatroom"
       });
+
+      socket.on('chat message', function(message) {
+        console.log(session.user.name + ' posted a message!')
+        io.emit('chat message', message);
+      });
+
+      socket.on('disconnect', function() {
+        console.log(session.user.name + ' left the chatroom!')
+        io.emit('chat message', {
+          user: session.user.name,
+          text: " left the chatroom"
+        });
+      });
+  
     });
 
   });
