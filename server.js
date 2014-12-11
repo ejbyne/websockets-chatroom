9af09570280642eed207e88f88b0292bfe9f8a27
ed <ejbyne@gmail.com>
2014-12-11 12:11:58 +0000

    initial set up not working

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var socket = require('./src/socketController.js')(io);
 
 app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/sockets', function(request, response){
-  response.render('sockets');
+  response.render('socket');
+});
+
+app.listen(3000, function() {
+  console.log("listening on port 3000");
 });
---
+++ src/socketController.js
@@ -0,0 +1,9 @@
+var socket = function(io) {
+
+  io.on('connection', function(){
+    console.log('Client connected!');
+  });
+
+}
+
+module.exports = socket;
