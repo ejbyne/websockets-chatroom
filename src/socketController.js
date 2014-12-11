9e10d0cc9c9225257610f1074b7dcc37a88ae2b6
ed <ejbyne@gmail.com>
2014-12-11 13:34:13 +0000

    added disconnect message

--- src/socketController.js
+++ src/socketController.js
@@ -1,7 +1,13 @@
 var socket = function(io) {
 
-  io.on('connection', function(){
+  io.on('connection', function(socket){
+    
     console.log('Client connected!');
+
+    socket.on('disconnect', function() {
+      console.log('Client disconnected!');
+    });
+
   });
 
 };
