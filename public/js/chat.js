cdac02087d02c1c1befb78851cfc265e289a9ef6
Alan Bridger <alanrbridger@gmail.com>
2014-12-11 16:48:30 +0000

    Chats now include user names

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,18 +1,24 @@
-$( document ).ready(function(){
+$(document).ready(function() {
   var socket = io.connect('/');
 
   $('#enter-name').submit(function() {
-    // var name = $('#name').val();
-  });
+    event.preventDefault();
+    var username = $('#name').val();
+    $('#name').val('');
+    $('#enter-name').fadeOut(500);
 
-  $('#chat-form').submit(function(){
-    socket.emit('chat message', $('#message-text').val());
-    $('#message-text').val('');
-    return false;
-  });
+    $('#chat-form').submit(function() {
+      socket.emit('chat message', {
+        user: username,
+        text: $('#message-text').val()
+      });
+      $('#message-text').val('');
+      return false;
+    });
 
-  socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
+    socket.on('chat message', function(msg) {
+      $('#messages').append($('<li>').text(msg.user + ": " + msg.text));
+    });
   });
 
 });
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var server = require('http').createServer(app);
 var bodyParser = require('body-parser');
 var io = require('socket.io')(server);
 var socket = require('./src/chatController.js')(io);
-var session;
+var session = require('express-session');
 
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
@@ -12,19 +12,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.get('/', function(request, response) {
-  session = request.session;
   response.render('index');
-});
-
-app.post('/', function(request, response) {
-  session.name = request.param.name;
-  console.log(session.name);
-})
-
-app.get('/socket', function(request, response){
-  response.render('socket');
 });
 
 server.listen(3000, function() {
